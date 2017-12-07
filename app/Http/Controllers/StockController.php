6167a7ff4b1b80b37579fb082add3b30ebe5f14c
Alinux13 <alinux1994@gmail.com>
2017-12-07 22:38:32 +0300

    add Product and list

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\StockRequest;
+use App\User;
+use App\Stock;
+use Auth;
 
 class StockController extends Controller
 {
-    //
+    public function __construct()
+    {
+      $this->middleware('auth');
+    }
+
+    public function stockPost(StockRequest $request)
+    {
+      $stock = Stock::create([
+
+        'user_id' => Auth::user()->id,
+        'product_name' => $request->product_name,
+        'quantity_in_stock' => $request->quantity_in_stock,
+        'price_per_item' => $request->price_per_item
+
+      ]);
+
+      $stock->save();
+      return redirect()->back()->with('status', 'Success');
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,10 +14,61 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <form action="" method="post" style="mrx-5">
+                      {{ csrf_field() }}
+                      <div class="form-group">
+                       <label for="email">Product Name:</label>
+                       <input type="text" class="form-control" name="product_name">
+                      </div>
+                      <div class="form-group">
+                       <label for="pwd">Quantity in stock:</label>
+                       <input type="text" class="form-control" name="quantity_in_stock">
+                      </div>
+                      <div class="form-group">
+                       <label for="pwd">Price per item:</label>
+                       <input type="text" class="form-control" name="price_per_item">
+                      </div>
+                      <button type="submit" class="btn btn-primary">Add</button>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">All Stocks</div>
+
+                <div class="panel-body">
+
+                      <div class="form-group">
+                        <table class="table table-striped">
+                         <thead>
+                           <tr>
+                             <th scope="col">ID</th>
+                             <th scope="col">User</th>
+                             <th scope="col">Product Name</th>
+                             <th scope="col">Quantity in stock</th>
+                             <th scope="col">Price per item</th>
+                           </tr>
+                         </thead>
+                         <tbody>
+                           @foreach ($stocks as $stock)
+                             <tr>
+                               <th scope="row">{{ $stock->id }}</th>
+                               <td>{{ $stock->User->name }}</td>
+                               <td>{{ $stock->product_name }}</td>
+                               <td>{{ $stock->quantity_in_stock }}</td>
+                               <td>{{ $stock->price_per_item }} $</td>
+                             </tr>
+                           @endforeach
+
+                         </tbody>
+                       </table>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
 </div>
 @endsection
